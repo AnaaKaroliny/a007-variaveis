9971bc341598d51ea7b187af05bf0e4c8d56bca0
Ana Karoliny <akaroliny98@gmail.com>
2022-07-19 17:24:49 -0300

    Exercicios realizados aula 007

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+let nome = prompt("Qual seu nome?");
+let idade = prompt("Quantos anos você tem?");
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+console.log("Olá", nome, "! Sua idade é", idade, "anos.");
---
+++ exercicio2/script.js
@@ -0,0 +1,14 @@
+const perguntaA = "Você gosta de pagode?";
+let pagodeiro = prompt(perguntaA);
+const perguntaB = "Você é do Rio de Janeiro?";
+let carioca = prompt(perguntaB);
+const perguntaC = "Você gosta de dançar?";
+let dancarino = prompt(perguntaC);
+
+let pagode = pagodeiro;
+let rio = carioca;
+let dancante = dancarino;
+
+console.log(perguntaA, "-", pagode);
+console.log(perguntaB, "-", rio);
+console.log(perguntaC, "-", dancante);
